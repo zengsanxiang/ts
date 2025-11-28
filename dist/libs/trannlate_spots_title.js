5474890aba61080a1712e9d910643af7afe6a597
曾三香 <116865771+zengsanxiang@users.noreply.github.com>
2025-11-28 10:52:56 +0800

    Update trannlate_spots_title.js

--- dist/libs/trannlate_spots_title.js
+++ dist/libs/trannlate_spots_title.js
@@ -19,6 +19,7 @@ const init = async () => {
     const list = await db_1.db.query.spots.findMany({
         where: (0, drizzle_orm_1.isNull)(schema_1.spots.title),
         orderBy: (0, drizzle_orm_1.asc)(schema_1.spots.c_at),
+        limit: 1000,
     });
     if (!list.length)
         return;
@@ -30,27 +31,31 @@ const init = async () => {
             continue; // 再兜底一次
         if (item?.title)
             continue; // 再兜底一次
-        const completion = await client.chat.completions.create({
-            model: "moonshot-v1-8k",
-            messages: [
-                {
-                    role: "system",
-                    content: "你是一个专业的旅游翻译，将中文翻译为英文。符合美国人的翻译习惯，只给翻译结果，不要有其他解释。",
-                },
-                { role: "user", content: item.title_cn },
-            ],
-            temperature: 0.6,
-        });
-        // console.log(completion);
-        // 安全取值
-        const content = completion.choices[0]?.message?.content;
-        if (content) {
-            // console.log(content)
-            await db_1.db
-                .update(schema_1.spots)
-                .set({ title: content })
-                .where((0, drizzle_orm_1.eq)(schema_1.spots.id, item.id));
+        try {
+            const completion = await client.chat.completions.create({
+                model: "moonshot-v1-8k",
+                messages: [
+                    {
+                        role: "system",
+                        content: "你是一个专业的旅游翻译，将中文翻译为英文。符合美国人的翻译习惯，只给翻译结果，不要有其他解释。",
+                    },
+                    { role: "user", content: item.title_cn },
+                ],
+                temperature: 0.6,
+            });
+            // console.log(completion);
+            // 安全取值
+            const content = completion.choices[0]?.message?.content;
+            if (content) {
+                // console.log(content)
+                await db_1.db
+                    .update(schema_1.spots)
+                    .set({ title: content })
+                    .where((0, drizzle_orm_1.eq)(schema_1.spots.id, item.id));
+            }
         }
+        catch (error) { }
     }
+    await init();
 };
 init().catch(console.error);
